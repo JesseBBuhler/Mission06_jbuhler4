daf201853e916afaf4a3a8a209c35f9481f7b941
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2023-02-13 23:07:36 -0700

    Setting up database connections

---
+++ Mission06_jbuhler4/Models/MovieDatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_jbuhler4.Models
+{
+    public class MovieDatabaseContext : DbContext
+    {
+        //Constructor
+        public MovieDatabaseContext (DbContextOptions<MovieDatabaseContext> options) : base (options)
+        {
+            //Leave Blank for now
+        }
+
+        public DbSet<Movies> movies { get; set; }
+    }
+}
